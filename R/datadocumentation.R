35053f8992d8210b2dc9338291a633c2850670c0
Noam Ross <noam.ross@gmail.com>
2012-11-19 10:59:17 -0800

    Made data easier to load
     - Put data into ";" delimited form
     - Moved to data folder
     - Added (incomplete) documentation
     - Deleted unused files

---
+++ R/datadocumentation.R
@@ -0,0 +1,9 @@
+#' Parameters to replicate Cobb (2012)
+#' 
+#' This data table contains the parameters to reproduce results from Cobb et al.
+#' (2012)
+#' 
+#' @docType data
+#' @format a CSV file
+#' @name parms.Cobb2012
+NULL
