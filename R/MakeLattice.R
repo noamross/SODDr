fdf46307b4ee1635093cbc24eb5ab3a71c3e7482
Noam Ross <noam.ross@gmail.com>
2013-05-19 13:37:01 -0700

    Fixing MaxLattice to match hex.pts
    Still need to fix MakeHexLattice.

--- R/MakeLattice.R
+++ R/MakeLattice.R
@@ -1,12 +1,13 @@
 #'Generate a lattice of equally spaced locations and coordinates
 #'@export
 MakeLattice <- function(nx, ny, dist=1, origin=c(0,0)) {
-  locations <- cbind(location = 1:(nx*ny), 
-                     x = rep(seq(from=0, by=dist, length.out=nx), each=ny) +
+  locations <- cbind(x = rep(seq(from=0, by=dist, length.out=nx), each=ny) +
                          origin[1],
                      y = rep(seq(from=0, by=dist, length.out=ny), times=nx) + 
                          origin[2]
   )
+  dimnames(locations) <- list(Location=1:nrow(locations),
+                              Coordinate=c("x","y"))
   class(locations) <- c(class(locations), "lattice")
   attr(locations, "gridsize") <- dist
   return(locations)
