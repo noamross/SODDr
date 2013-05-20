7b55673572e124cb86e8c56dd8bc5e0fbbe1d28f
Noam Ross <noam.ross@gmail.com>
2013-05-19 18:36:41 -0700

    Sped up strsplit operation in melt.SODD
    Used the data.table package to drastically speed up
    strsplit.

--- R/melt.SODD.R
+++ R/melt.SODD.R
@@ -1,14 +1,20 @@
 #'Convert SODD output to a data frame
 #'@export
-#'@import reshape2
+#'@import reshape2 data.table
 melt.SODD <- function(data, varnames=names(dimnames(data)),na.rm=FALSE,
                       value.name="Population") {
-  data.df <- reshape2:::melt.array(data, varnames, na.rm=na.rm, 
+  data.df <- reshape2:::melt.array(data=data, varnames=varnames, na.rm=na.rm,
                                    value.name=value.name)
-  cl <- which(names(data.df)=="Class")
-  Classes <- do.call(rbind, strsplit(as.character(data.df$Class), "\\."))
-  colnames(Classes) <- c("Species", "SizeClass", "Infected")
-  data.df <- cbind(data.df[,1:(cl-1)],Classes,data.df[(cl+1):(ncol(data.df))])
+  DT <- data.table(data.df)
+  DT[, c("Species", "SizeClass", "Infected")  := as.list(strsplit(as.character(Class), "\\.")[[1]]), by=Class ]
+  data.df <- as.data.frame(DT)
+  data.df$Species <- as.factor(data.df$Species)
+  data.df$SizeClass <- as.factor(data.df$SizeClass)
+  data.df$Infected <- as.factor(data.df$Infected)
+  data.df <- data.df[,c(1,2,4,6,7,8,5)]
   return(data.df)
 }
   
+
+library(data.table)
+
