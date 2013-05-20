fc7345eb34898b350fa5028d7e6c279d8b8f7895
Noam Ross <noam.ross@gmail.com>
2013-05-20 09:10:35 -0700

    Changed melt.SODD to output data.table (not frame)

--- R/melt.SODD.R
+++ R/melt.SODD.R
@@ -3,18 +3,16 @@
 #'@import reshape2 data.table
 melt.SODD <- function(data, varnames=names(dimnames(data)),na.rm=FALSE,
                       value.name="Population") {
-  data.df <- reshape2:::melt.array(data=data, varnames=varnames, na.rm=na.rm,
-                                   value.name=value.name)
-  DT <- data.table(data.df)
-  DT[, c("Species", "SizeClass", "Infected")  := as.list(strsplit(as.character(Class), "\\.")[[1]]), by=Class ]
-  data.df <- as.data.frame(DT)
-  data.df$Species <- as.factor(data.df$Species)
-  data.df$SizeClass <- as.factor(data.df$SizeClass)
-  data.df$Infected <- as.factor(data.df$Infected)
-  data.df <- data.df[,c(1,2,4,6,7,8,5)]
-  return(data.df)
+  DT <- data.table(reshape2:::melt.array(data=data, varnames=varnames, 
+                                              na.rm=na.rm,
+                                              value.name=value.name))
+  newCols <- c("Species", "SizeClass", "Infected") 
+  DT[, c(newCols) := as.list(strsplit(as.character(Class), "\\.")[[1]]),
+     by=Class ]
+  DT[, c(newCols) := lapply(.SD, factor), .SDcols=newCols]
+  DT[, Class := NULL]
+  setcolorder(DT,c(1,2,3,5,6,7,4))
+  return(DT)
 }
   
-
-library(data.table)
 
